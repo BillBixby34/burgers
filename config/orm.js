ac11ea03050b331727aac9c0b87d458ae591b8b5
Will H <wj_hilliard@yahoo.com>
2017-08-26 09:29:58 -0400

    can set server, next views

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,13 @@
 //Import (require) `connection.js` into `orm.js`
 var connection = require("./connection.js");
 //methods that will execute mysql commands
+function selectAll(){};
+
+function insertOne(){};
+
+function updateOne(){};
+
+
 var orm;
 //* `selectAll()` 
 //   * `insertOne()` 
--- server.js
+++ server.js
@@ -2,27 +2,24 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-
+var port = process.env.PORT || process.argv[2] || 3000;
 var app = express();
-//sets port variable;
-app.set('port', process.env.PORT || process.argv[2] || 3000);
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
+
 var exphbs = require("express-handlebars");
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: 'hbs'}));
-app.set("view engine", "hbs");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 //check app.set(...) listen does not work
 
 //do I import routes here so server may access
 var routes = require("./controllers/burgers_controller.js");
 
-
-
-
-
-
+//change to app.listen(port) => no errors
 app.listen(app.get('port'),() => console.log("Listening on PORT " + port)
 );

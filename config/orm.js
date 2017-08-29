f56438362739ccf970ecacb535cfa864739b0556
Will H <wj_hilliard@yahoo.com>
2017-08-29 17:05:30 -0400

    filling out orm, connectors, views

--- config/orm.js
+++ config/orm.js
@@ -8,7 +8,37 @@ function insertOne(){};
 function updateOne(){};
 
 
-var orm;
+var orm = {
+	selectAll: function(tableInput, cb) {
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		connection.query(queryString, function(err, result){
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+insertOne: function(table, cols, vals, cb) {
+	var queryString = "INSERT INTO " + table;
+},
+	//objColVals will be {burger_name: cheese, devoured: false}
+	updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+}
 //* `selectAll()` 
 //   * `insertOne()` 
 // * `updateOne()` 
--- models/burger.js
+++ models/burger.js
@@ -1,31 +1,31 @@
-//import `orm.js` into `burger.js`
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 //inside `burger.js`, create the code that will call 
 //ORM functions(* `selectAll()`, * `insertOne()`, * `updateOne()` ) using burger specific input for the ORM.
 
 var burger = {
-  all: function(cb) {
+  selectAll: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
+  insertOne: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
+  updateOne: function(objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+  }//add comma when uncomment ,
+  //delete: function(condition, cb) {
+    //orm.delete("burgers", condition, function(res) {
+    //  cb(res);
+    //});
+  //}
 };
 
 module.exports = burger;

ad9703212c4eefc027478f8a3c0136cd1b2dfdfa
Will H <wj_hilliard@yahoo.com>
2017-09-03 11:58:13 -0400

    JAWS_DB changes

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 //is having port in connection object redundant?
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	//should I take 'port: 3306' out?
+ connection = mysql.createConnection({
 	port: 3306,
 	host: "localhost",
 	user: "root",
 	password: "1001",
 	database: "burgers_db"
 });
-
+}
 //Inside the `connection.js` file, setup the code to connect Node to MySQL.
 connection.connect(function(err){
 	if (err){
